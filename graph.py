70f00f12bf18456173503a92db2014370ee6eb12
davidg <davidgong99@gmail.com>
2020-08-03 15:37:37 +1000

    remove unused functions

--- graph.py
+++ graph.py
@@ -11,18 +11,18 @@ class Graph():
     ''' Check if this vertex is an adjacent vertex  
         of the previously added vertex and is not  
         included in the path earlier '''
-    def isSafe(self, v, pos, path):  
-        # Check if current vertex and last vertex  
-        # in path are adjacent  
-        if self.graph[ path[pos-1] ][v] == 0:  
-            return False
+    # def isSafe(self, v, pos, path):  
+    #     # Check if current vertex and last vertex  
+    #     # in path are adjacent  
+    #     if self.graph[ path[pos-1] ][v] == 0:  
+    #         return False
   
-        # Check if current vertex not already in path  
-        for vertex in path:  
-            if vertex == v:  
-                return False
+    #     # Check if current vertex not already in path  
+    #     for vertex in path:  
+    #         if vertex == v:  
+    #             return False
   
-        return True
+    #     return True
 
     # need to make dictionary
     # key = node/vertex number (int)
@@ -160,51 +160,51 @@ class Graph():
 
     # A recursive utility function to solve  
     # hamiltonian cycle problem  
-    def hamCycleUtil(self, path, pos):  
+    # def hamCycleUtil(self, path, pos):  
   
-        # base case: if all vertices are  
-        # included in the path  
-        if pos == self.V:  
-            # Last vertex must be adjacent to the  
-            # first vertex in path to make a cyle  
-            if self.graph[ path[pos-1] ][ path[0] ] == 1:  
-                return True
-            else:  
-                return False
+    #     # base case: if all vertices are  
+    #     # included in the path  
+    #     if pos == self.V:  
+    #         # Last vertex must be adjacent to the  
+    #         # first vertex in path to make a cyle  
+    #         if self.graph[ path[pos-1] ][ path[0] ] == 1:  
+    #             return True
+    #         else:  
+    #             return False
   
-        # Try different vertices as a next candidate  
-        # in Hamiltonian Cycle. We don't try for 0 as  
-        # we included 0 as starting point in hamCycle()  
-        for v in range(1,self.V):  
+    #     # Try different vertices as a next candidate  
+    #     # in Hamiltonian Cycle. We don't try for 0 as  
+    #     # we included 0 as starting point in hamCycle()  
+    #     for v in range(1,self.V):  
   
-            if self.isSafe(v, pos, path) == True:  
+    #         if self.isSafe(v, pos, path) == True:  
   
-                path[pos] = v  
+    #             path[pos] = v  
   
-                if self.hamCycleUtil(path, pos+1) == True:  
-                    return True
+    #             if self.hamCycleUtil(path, pos+1) == True:  
+    #                 return True
   
-                # Remove current vertex if it doesn't  
-                # lead to a solution  
-                path[pos] = -1
+    #             # Remove current vertex if it doesn't  
+    #             # lead to a solution  
+    #             path[pos] = -1
   
-        return False
+    #     return False
   
-    def hamCycle(self):  
-        path = [-1] * self.V  
+    # def hamCycle(self):  
+    #     path = [-1] * self.V  
   
-        ''' Let us put vertex 0 as the first vertex  
-            in the path. If there is a Hamiltonian Cycle,  
-            then the path can be started from any point  
-            of the cycle as the graph is undirected '''
-        path[0] = 0
+    #     ''' Let us put vertex 0 as the first vertex  
+    #         in the path. If there is a Hamiltonian Cycle,  
+    #         then the path can be started from any point  
+    #         of the cycle as the graph is undirected '''
+    #     path[0] = 0
   
-        if self.hamCycleUtil(path,1) == False:  
-            print ("Solution does not exist\n") 
-            return False
+    #     if self.hamCycleUtil(path,1) == False:  
+    #         print ("Solution does not exist\n") 
+    #         return False
   
-        self.printSolution(path)  
-        return True
+    #     self.printSolution(path)  
+    #     return True
   
     def printSolution(self, path):  
         print ("Solution Exists: Following", 
