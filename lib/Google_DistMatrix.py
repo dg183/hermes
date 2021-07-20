0a093bf67c811cb281b0d4da32e67c5f23a72223
David Gong <davidgong99@gmail.com>
2021-07-20 16:28:21 +1000

    add matrix operation static methods

--- lib/Google_DistMatrix.py
+++ lib/Google_DistMatrix.py
@@ -64,3 +64,65 @@ class Google_DistMatrix:
             result = json.load(response)
             print(result)
         
+    # Given a response from the Distance Matrix API
+    # Output the matrix of paths between each node
+    @staticmethod
+    def print_matrix(api_response):
+        # res = json.loads(api_response)
+        nodes = api_response["destination_addresses"]
+        rows = api_response["rows"]
+        
+        # print(nodes)
+        # print(json.dumps(rows, indent=4))
+        
+        
+        for row in rows:
+            # print(row["elements"])
+            for col in row["elements"]:
+                # print(col)
+                print(f'{col["distance"]["text"]},{col["duration"]["text"]}',end="")
+                print(" | ",end="")
+                
+            print("<")
+
+    # Given a response from Distance Matrix API
+    # Return matrix of the travel times between nodes
+    # Units = Seconds
+    @staticmethod
+    def matrix_by_time(api_response):
+        rows = api_response["rows"]
+
+        matrix = [[0 for x in range(len(rows))] for y in range(len(rows))]
+        
+        row_count = 0
+        col_count = 0
+        
+        for row in rows:
+            col_count = 0
+            for col in row["elements"]:
+                matrix[row_count][col_count] = col["duration"]["value"]
+                col_count += 1
+            row_count += 1
+            
+        return matrix
+
+    # Given a response from Distance Matrix API
+    # Return matrix of the distances between nodes
+    # Units = Metres
+    @staticmethod
+    def matrix_by_distance(api_response):
+        rows = api_response["rows"]
+
+        matrix = [[0 for x in range(len(rows))] for y in range(len(rows))]
+        
+        row_count = 0
+        col_count = 0
+        
+        for row in rows:
+            col_count = 0
+            for col in row["elements"]:
+                matrix[row_count][col_count] = col["distance"]["value"]
+                col_count += 1
+            row_count += 1
+            
+        return matrix
